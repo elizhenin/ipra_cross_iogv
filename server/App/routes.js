1f8f56cf678e2d9df9e2bde676bc630d314fde0a
E.Lizhenin <elizhenin@lsbr.ru>
2017-05-28 01:45:27 +0300

    simple routing in server

--- server/App/routes.js
+++ server/App/routes.js
@@ -1,5 +1,25 @@
 exports.init = function(app) {
-    app.get('/', function (req, res) {
-        res.send('Hello World!');
+    app.all('/*', function(req, res) {
+        var URI_path = req.params[0];
+        URI_path = URI_path.split('/');
+        var result = 'Error case';
+        switch (URI_path[0]) {
+            case '':
+                {
+                    result = "Start case";
+                    break;
+                };
+            case 'admin':
+                {
+                    result = "Admin case";
+                    break;
+                };
+            case 'user':
+                {
+                    result = "User case";
+                    break;
+                }
+        }
+        res.send(result);
     });
 };
